3250314d8d10111ea3e4e51d41425d9f43152723
HeberIniguez <126428820+HeberIniguez@users.noreply.github.com>
2025-02-01 14:04:24 -0800

    Se agrega la funcionalidad de Sortear amigo

--- app.js
+++ app.js
@@ -4,15 +4,17 @@
 //Variables
 let amigos = [];
 let lista = document.getElementById('listaAmigos');
+let listaResultado = document.getElementById('resultado');
 let insertarAmigo = document.getElementById('amigo');
 
 function limpiarLista(){
     lista.innerHTML="";
+    listaResultado.innerHTML="";
 }
 
 //Funcion para agregar amigos a la lista.
 function agregarAmigo(){
-    
+
     //Comprueba si el campo esta vacio.
     if(insertarAmigo.value != ""){
         amigos.push(insertarAmigo.value);
@@ -21,10 +23,9 @@ function agregarAmigo(){
         alert("Por favor ingrese un nombre, no puede estar vacio.");
     }
 
-    //Limpia la lista, evitando duplicados.
     limpiarLista();
     
-    //Crea y agrega los nombres a la lista.
+    //Agrega los nombres a la lista.
     for(let amigo in amigos){
         let li = document.createElement("li");
         li.textContent = amigos[amigo];
@@ -33,4 +34,12 @@ function agregarAmigo(){
 
     //Mantiene el foco en el recuadro de texto.
     insertarAmigo.focus();
+}
+
+//Selecciona un nombre aleatorio y lo muestra en la lista.
+function sortearAmigo(){
+    let li = document.createElement("li");
+    limpiarLista();
+    li.textContent = amigos[Math.floor(Math.random()*amigos.length)];
+    listaResultado.appendChild(li);
 }
