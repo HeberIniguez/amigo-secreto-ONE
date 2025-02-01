c3e91f18c7339cfa0ee224e1a9455f32a3cb3433
HeberIniguez <126428820+HeberIniguez@users.noreply.github.com>
2025-02-01 13:20:57 -0800

    Empezamos la logica para el boton agregar

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+    Aquí deberás desarrollar la lógica para resolver el problema.*/
+
+//Variables
+let amigos = [];
+let lista = document.getElementById('listaAmigos');
+let insertarAmigo = document.getElementById('amigo');
+
+function limpiarLista(){
+    lista.innerHTML="";
+}
+
+//Funcion para agregar amigos a la lista.
+function agregarAmigo(){
+    
+    //Comprueba si el campo esta vacio.
+    if(insertarAmigo.value != ""){
+        amigos.push(insertarAmigo.value);
+        insertarAmigo.value = "";
+    }else{
+        alert("Por favor ingrese un nombre, no puede estar vacio.");
+    }
+
+    //Limpia la lista, evitando duplicados.
+    limpiarLista();
+    
+    //Crea y agrega los nombres a la lista.
+    for(let amigo in amigos){
+        let li = document.createElement("li");
+        li.textContent = amigos[amigo];
+        lista.appendChild(li);
+    }
+
+    //Mantiene el foco en el recuadro de texto.
+    insertarAmigo.focus();
+}
